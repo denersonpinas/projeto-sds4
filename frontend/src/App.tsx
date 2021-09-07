b980f1647c831b3f1b2f2b6edf097e35ca658db2
denersonpinas <denersonpinas19@gmail.com>
2021-09-06 23:37:25 -0300

    Update title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
             <BarChart/>
           </div>
           <div className="col-sm-6" >
-          <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+          <h5 className="text-center text-secondary">Taxa de vendas</h5>
             <DonutChart/>
           </div>
         </div>
