bf7eb5b059f867c79b02f76b93c2b059c52318ea
denersonpinas <denersonpinas19@gmail.com>
2021-09-09 10:46:13 -0300

    Group by search

--- backend/src/main/java/com/denersonpinas/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/denersonpinas/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.denersonpinas.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.denersonpinas.dsvendas.dto.SaleDTO;
+import com.denersonpinas.dsvendas.dto.SaleSuccessDTO;
+import com.denersonpinas.dsvendas.dto.SaleSumDTO;
 import com.denersonpinas.dsvendas.entities.Sale;
 import com.denersonpinas.dsvendas.repositories.SaleRepository;
 import com.denersonpinas.dsvendas.repositories.SellerRepository;
@@ -26,4 +30,14 @@ public class SaleService {
 		Page<Sale> result = repository.findAll(pegeable);
 		return result.map(x -> new SaleDTO(x));
 	}	
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
+	}
 }
