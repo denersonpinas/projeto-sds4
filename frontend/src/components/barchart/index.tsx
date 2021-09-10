43a6876c9fb4c7d9ca79d177c305af2f622c28a6
denersonpinas <denersonpinas19@gmail.com>
2021-09-10 11:21:03 -0300

    BarChart integration

--- frontend/src/components/barchart/index.tsx
+++ frontend/src/components/barchart/index.tsx
@@ -1,6 +1,59 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/requests';
+
+type SeriesData = {
+    name: string;
+    data: number[];
+}
+
+type ChartData = {
+    labels: {
+        categories: string[];
+    };
+    series: SeriesData[];
+}
 
 function BarChart() {
+
+    const [chartData, setChartData] = useState<ChartData>({
+        labels: {
+            categories: []
+        },
+        // A nossa estrutura SERIES, aceita mais que um parametro
+        series: [
+            {
+                name: "",
+                data: []                   
+            }
+        ]
+    })
+
+    useEffect(() => {
+        axios.get(BASE_URL + '/sales/success-by-seller')
+             .then(response => {
+                 const data = response.data as SaleSuccess[];
+                 const myLabels = data.map(x => x.sellerName)
+                 const mySeries = data.map(x => round(100 * x.deals / x.visited, 1))
+                 
+                 setChartData({
+                    labels: {
+                        categories: myLabels
+                    },
+                    // A nossa estrutura SERIES, aceita mais que um parametro
+                    series: [
+                        {
+                            name: "(%) Sucesso",
+                            data: mySeries                   
+                        }
+                    ]
+                })
+             })
+    })
+
     const options = {
         plotOptions: {
             bar: {
@@ -9,23 +62,23 @@ function BarChart() {
         },
     };
     
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        // A nossa estrutura SERIES, aceita mais que um parametro
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
-            }
-        ]
-    };
+    // const mockData = {
+    //     labels: {
+    //         categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //     },
+    //     // A nossa estrutura SERIES, aceita mais que um parametro
+    //     series: [
+    //         {
+    //             name: "% Sucesso",
+    //             data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
+    //         }
+    //     ]
+    // };
 
     return (
         <Chart 
-            options={{...options, xaxis: mockData.labels}} // XAXIS = É onde fica nossas labels do eixo X
-            series={mockData.series} // SERIES = É onde fica os valores das labels 
+            options={{...options, xaxis: chartData.labels}} // XAXIS = É onde fica nossas labels do eixo X
+            series={chartData.series} // SERIES = É onde fica os valores das labels 
             type="bar"
             height="240"
         />
